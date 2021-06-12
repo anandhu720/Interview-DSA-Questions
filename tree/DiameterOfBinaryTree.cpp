77aca37944a2b6e634178d14a59b4ffc6d6a56eb
anandhu720 <anandhu4310@gmail.com>
2021-06-12 20:11:06 +0530

    new question

---
+++ tree/DiameterOfBinaryTree.cpp
@@ -0,0 +1,61 @@
+/*
+ *
+ * Given a Binary Tree, find diameter of it.
+The diameter of a tree is the number of nodes on the longest path between two end nodes in the tree. The diagram below shows two trees each with diameter nine, the leaves that form the ends of a longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).
+
+
+
+Example 1:
+
+Input:
+       1
+     /  \
+    2    3
+Output: 3
+Example 2:
+
+Input:
+         10
+        /   \
+      20    30
+    /   \
+   40   60
+Output: 4
+
+link of question :https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1#
+*/
+
+
+class Solution {
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int height(Node* root){
+        int x=0,y=0;
+        if(root == 0)
+            return 0;
+        x=height(root->left);
+        y=height(root->right);
+
+        return (x>y)?x+1:y+1;
+    }
+
+    int max(int x,int y){
+        return (x>y)?x:y;
+    }
+
+    int diameter(Node* root) {
+        // Your code here
+
+        if(root == NULL)
+            return 0;
+
+        int lheight = height(root->left);
+        int rheight = height(root->right);
+
+        int ldiameter = diameter(root->left);
+        int rdiameter = diameter(root->right);
+
+        return max(lheight+rheight+1,max(ldiameter,rdiameter));
+    }
+};
+
