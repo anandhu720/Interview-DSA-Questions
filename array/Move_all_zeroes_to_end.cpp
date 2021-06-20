8cea5cd20cb2828f1081beab48f26f94a21a906b
anandhu720 <anandhu4310@gmail.com>
2021-06-20 22:36:13 +0530

    new question

--- array/Move_all_zeroes_to_end.cpp
+++ array/Move_all_zeroes_to_end.cpp
@@ -3,13 +3,6 @@ Given an array arr[] of N positive integers.
 Push all the zero’s of the given array to the right end of the array while maitaining the order of non-zero elements.
 */
 
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-
-//User function template for C++
 class Solution
 {
 public:
@@ -32,27 +25,4 @@ public:
     }
 };
 
-// { Driver Code Starts.
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n, i;
-        cin >> n;
-        int arr[n];
-        for (i = 0; i < n; i++)
-        {
-            cin >> arr[i];
-        }
-        Solution ob;
-        ob.pushZerosToEnd(arr, n);
-        for (i = 0; i < n; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-} // } Driver Code Ends
+
