e03779d5b42e9120cbb1b320bd2221a18a1f47af
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2021-07-11 11:14:30 +0530

    Update even_and_odd.cpp

--- array/even_and_odd.cpp
+++ array/even_and_odd.cpp
@@ -4,13 +4,7 @@ Arrange the numbers in such a way that all the even numbers get the even index a
 Note: There are multiple possible solutions, Print any one of them. Also, 0-based indexing is considered.
 */
 
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
-// } Driver Code Ends
-
-//User function Template for C++
 
 class Solution
 {
@@ -42,58 +36,3 @@ public:
     }
 };
 
-// { Driver Code Starts.
-
-int check(int arr[], int n)
-{
-    int flag = 1;
-    int c = 0, d = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (i % 2 == 0)
-        {
-            if (arr[i] % 2)
-            {
-                flag = 0;
-                break;
-            }
-            else
-                c++;
-        }
-        else
-        {
-            if (arr[i] % 2 == 0)
-            {
-                flag = 0;
-                break;
-            }
-            else
-                d++;
-        }
-    }
-    if (c != d)
-        flag = 0;
-
-    return flag;
-}
-
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int N;
-        cin >> N;
-
-        int arr[N];
-        for (int i = 0; i < N; i++)
-            cin >> arr[i];
-
-        Solution ob;
-        ob.reArrange(arr, N);
-
-        cout << check(arr, N) << endl;
-    }
-    return 0;
-} // } Driver Code Ends
