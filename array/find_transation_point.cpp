c0347c90bf83e8d378cf4366cb0894e200e25740
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2021-07-12 10:32:32 +0530

    Update find_transation_point.cpp

--- array/find_transation_point.cpp
+++ array/find_transation_point.cpp
@@ -2,30 +2,6 @@
 Given a sorted array containing only 0s and 1s, find the transition point. 
 */
 
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-int transitionPoint(int arr[], int n);
-
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        int a[n], i;
-        for (i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        cout << transitionPoint(a, n) << endl;
-    }
-    return 0;
-} // } Driver Code Ends
-
 int transitionPoint(int arr[], int n)
 {
     // code here
