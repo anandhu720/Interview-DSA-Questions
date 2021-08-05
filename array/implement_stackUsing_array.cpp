38a5265fbc20c58552f98f59dd5c2c1829c7b7f6
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2021-08-05 21:24:01 +0530

    Update implement_stackUsing_array.cpp

--- array/implement_stackUsing_array.cpp
+++ array/implement_stackUsing_array.cpp
@@ -1,56 +1,7 @@
 // Write a program to implement a Stack using Array.
 // Your task is to use the class as shown in the comments in the code editor and complete the functions push() and pop() to implement a stack.
 
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
-class MyStack
-{
-private:
-    int arr[1000];
-    int top;
-
-public:
-    MyStack() { top = -1; }
-    int pop();
-    void push(int);
-};
-
-int main()
-{
-
-    int T;
-    cin >> T;
-    while (T--)
-    {
-        MyStack *sq = new MyStack();
-
-        int Q;
-        cin >> Q;
-        while (Q--)
-        {
-            int QueryType = 0;
-            cin >> QueryType;
-            if (QueryType == 1)
-            {
-                int a;
-                cin >> a;
-                sq->push(a);
-            }
-            else if (QueryType == 2)
-            {
-                cout << sq->pop() << " ";
-            }
-        }
-        cout << endl;
-    }
-}
-// } Driver Code Ends
-
-/* 
-
-The structure of the class is
 class MyStack
 {
 private:
@@ -83,7 +34,7 @@ void MyStack ::push(int x)
   poped out of the stack*/
 int MyStack ::pop()
 {
-    // Your Code
+    
     if (top == -1)
     {
         return -1;
